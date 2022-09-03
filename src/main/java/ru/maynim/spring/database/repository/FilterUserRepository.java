ba6cad83af2ba4d653ae3755aaf7f01f393c0f99
Dmitry Maslov <Maynim@yandex.ru>
2022-09-03 20:36:41 +0300

    Example query

--- src/main/java/ru/maynim/spring/database/repository/FilterUserRepository.java
+++ src/main/java/ru/maynim/spring/database/repository/FilterUserRepository.java
@@ -1,6 +1,8 @@
 package ru.maynim.spring.database.repository;
 
+import ru.maynim.spring.database.entity.Role;
 import ru.maynim.spring.database.entity.User;
+import ru.maynim.spring.dto.PersonalInfo;
 import ru.maynim.spring.dto.UserFilter;
 
 import java.util.List;
@@ -8,4 +10,6 @@ import java.util.List;
 public interface FilterUserRepository {
 
     List<User> findAllByFilter(UserFilter filter);
+
+    List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
 }
--- src/main/java/ru/maynim/spring/database/repository/FilterUserRepositoryImpl.java
+++ src/main/java/ru/maynim/spring/database/repository/FilterUserRepositoryImpl.java
@@ -3,8 +3,11 @@ package ru.maynim.spring.database.repository;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQuery;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.maynim.spring.database.entity.Role;
 import ru.maynim.spring.database.entity.User;
 import ru.maynim.spring.database.querydsl.QPredicates;
+import ru.maynim.spring.dto.PersonalInfo;
 import ru.maynim.spring.dto.UserFilter;
 
 import javax.persistence.EntityManager;
@@ -16,20 +19,37 @@ import static ru.maynim.spring.database.entity.QUser.user;
 public class FilterUserRepositoryImpl implements FilterUserRepository {
 
     private final EntityManager entityManager;
+    private final JdbcTemplate jdbcTemplate;
+
+    private static final String FIND_BY_COMPANY_AND_ROLE =
+        "SELECT firstname, lastname, birth_date " +
+            "FROM users " +
+            "WHERE company_id = ? " +
+            "AND role = ?";
 
     @Override
     public List<User> findAllByFilter(UserFilter filter) {
         Predicate predicate = QPredicates.builder()
-                .add(filter.firstname(), user.firstname::containsIgnoreCase)
-                .add(filter.lastname(), user.lastname::containsIgnoreCase)
-                .add(filter.birthDate(), user.birthDate::before)
-                .build();
+            .add(filter.firstname(), user.firstname::containsIgnoreCase)
+            .add(filter.lastname(), user.lastname::containsIgnoreCase)
+            .add(filter.birthDate(), user.birthDate::before)
+            .build();
 
         return new JPAQuery<User>(entityManager)
-                .select(user)
-                .from(user)
-                .where(predicate)
-                .fetch();
+            .select(user)
+            .from(user)
+            .where(predicate)
+            .fetch();
+    }
+
+    @Override
+    public List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role) {
+        return jdbcTemplate.query(FIND_BY_COMPANY_AND_ROLE,
+            (rs, rowNum) -> new PersonalInfo(
+                rs.getString("firstname"),
+                rs.getString("lastname"),
+                rs.getDate("birth_date").toLocalDate()
+            ), companyId, role.name());
     }
 
 
